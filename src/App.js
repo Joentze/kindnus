e5232678aee2414f9938859eaa7c8bb566dfd0ac
Joentze <Joentze01@gmail.com>
2022-01-07 15:38:12 +0800

    added feelings page, feelings button array

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import SendMessage from './pages/SendMessage';
+import Feelings from './pages/Feelings'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 function App() {
   return (
     <Router>
       <Routes>
         <Route exact path = "/" element={<SendMessage/>}/>
+        <Route exact path = "/feelings" element={<Feelings/>}/>
       </Routes>
     </Router>
   );
---
+++ src/components/EmotionButtonArray.js
@@ -0,0 +1,59 @@
+import { allEmotions, emotionsNumMap, emotionsMap } from "./misc/content"
+import JSConfetti from "js-confetti"
+import { useState } from "react"
+import db from "../config";
+import CircularProgress from '@mui/material/CircularProgress';
+
+const EmotionButtonArray =()=>{
+    const confetti = new JSConfetti()
+    const [feelSent, setFeelSent] = useState(false)
+    const writeFeelingToFB =(feeling)=>{
+        if(!feelSent){
+            let writeObj={
+                emotion:emotionsNumMap[feeling],
+                timestamp:new Date()
+            }
+            db.collection("emotions").add(writeObj).then(()=>{
+                setFeelSent(true)
+            }).catch((error)=>{
+                console.error(error)
+            })
+    }}
+    return (
+        <>{
+            feelSent?
+            (
+            <div className={"centerContent loadingBoxCircular"}>
+                <CircularProgress
+                    style={{
+                        color:"#5959ff"
+                    }}
+                />
+            </div>
+            )
+            :(<div>
+                {
+                    allEmotions.map((item, key)=>{
+                        return(
+                        <div key={key}>
+                            <button 
+                            className={"emotionSelectButton"} 
+                            key={key}
+                            onClick={()=>{
+                                confetti.addConfetti({
+                                    emojis:[emotionsMap[item]]
+                                })
+                                writeFeelingToFB(item)
+                            }}
+                            >
+                            <h3>&nbsp;{emotionsMap[item]}&nbsp;&nbsp;&nbsp;{item}</h3>
+                            </button>
+                        </div>
+                        )
+                    })
+                }
+            </div>)
+}</>
+    )
+}
+export default EmotionButtonArray
