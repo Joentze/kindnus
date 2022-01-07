d77889a35cf5ff0ed03e6a09017ce720f1de44bf
nathanielcalimag <calimagnathaniel@gmail.com>
2022-01-07 22:30:35 +0800

    Remove x-axis from heatmap due to time constraints

---
+++ src/components/MoodContainer.js
@@ -0,0 +1,30 @@
+import { Divider } from "@mui/material"
+import { allEmotions } from "./misc/content"
+import Chip from '@mui/material/Chip';
+const MoodContainer =({content, mood})=>{
+    const definition = content['definition']
+    const synonyms = content['synonyms']
+    return (
+        <div className={"centerContent moodDescriptionContainer"}>
+            <h1>{allEmotions[mood]}</h1>
+            {synonyms.map((item, key)=>{
+                return (<Chip 
+                    key={key} 
+                    label={item} 
+                    style={{
+                        margin:'5px', 
+                        fontFamily:'Nunito',
+                        background:'#ffdfbd',
+                        color:'#bf8649',
+                    }} 
+                    size="small"
+                    />)
+            })} 
+            <br></br>
+            <br></br>
+            <Divider/>
+            <p>{definition}</p>
+        </div>
+    )
+}
+export default MoodContainer
